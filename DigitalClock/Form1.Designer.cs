2f91b1730aef5ca7ac492d49ae16af62f93a139f
unknown <bulgarianmapper64@gmail.com>
2022-07-08 23:51:27 +0300

    First Commit

---
+++ DigitalClock/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace DigitalClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.appName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelMinutes = new System.Windows.Forms.Label();
+            this.labelSeconds = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelHours = new System.Windows.Forms.Label();
+            this.timerSeconds = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // appName
+            // 
+            this.appName.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.appName.Location = new System.Drawing.Point(-4, 9);
+            this.appName.Name = "appName";
+            this.appName.Size = new System.Drawing.Size(465, 42);
+            this.appName.TabIndex = 0;
+            this.appName.Text = "Digital Clock";
+            this.appName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 0;
+            // 
+            // labelMinutes
+            // 
+            this.labelMinutes.AutoSize = true;
+            this.labelMinutes.Font = new System.Drawing.Font("Microsoft JhengHei", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMinutes.Location = new System.Drawing.Point(217, 104);
+            this.labelMinutes.Name = "labelMinutes";
+            this.labelMinutes.Size = new System.Drawing.Size(162, 120);
+            this.labelMinutes.TabIndex = 3;
+            this.labelMinutes.Text = "00";
+            // 
+            // labelSeconds
+            // 
+            this.labelSeconds.AutoSize = true;
+            this.labelSeconds.Font = new System.Drawing.Font("Microsoft JhengHei", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSeconds.Location = new System.Drawing.Point(353, 180);
+            this.labelSeconds.Name = "labelSeconds";
+            this.labelSeconds.Size = new System.Drawing.Size(59, 44);
+            this.labelSeconds.TabIndex = 4;
+            this.labelSeconds.Text = "00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft JhengHei", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(160, 95);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 120);
+            this.label6.TabIndex = 5;
+            this.label6.Text = ":";
+            // 
+            // labelHours
+            // 
+            this.labelHours.AutoSize = true;
+            this.labelHours.Font = new System.Drawing.Font("Microsoft JhengHei", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHours.Location = new System.Drawing.Point(12, 104);
+            this.labelHours.Name = "labelHours";
+            this.labelHours.Size = new System.Drawing.Size(162, 120);
+            this.labelHours.TabIndex = 6;
+            this.labelHours.Text = "00";
+            // 
+            // timerSeconds
+            // 
+            this.timerSeconds.Interval = 1000;
+            this.timerSeconds.Tick += new System.EventHandler(this.timerSeconds_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 262);
+            this.Controls.Add(this.labelHours);
+            this.Controls.Add(this.labelSeconds);
+            this.Controls.Add(this.labelMinutes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.appName);
+            this.Controls.Add(this.label6);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormLoad);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label appName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelMinutes;
+        private System.Windows.Forms.Label labelSeconds;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelHours;
+        private System.Windows.Forms.Timer timerSeconds;
+    }
+}
+
---
+++ DigitalClock/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace DigitalClock
+{
+    public partial class Form1 : Form
+    {
+        private static System.Timers.Timer aTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLoad(object sender, EventArgs e)
+        {
+            labelHours.Text = DateTime.Now.Hour.ToString("00");
+            labelMinutes.Text = DateTime.Now.Minute.ToString("00");
+            labelSeconds.Text = DateTime.Now.Second.ToString("00");
+            timerSeconds.Start();
+        }
+
+        private void timerSeconds_Tick(object sender, EventArgs e)
+        {
+            labelHours.Text = DateTime.Now.Hour.ToString("00");
+            labelMinutes.Text = DateTime.Now.Minute.ToString("00");
+            labelSeconds.Text = DateTime.Now.Second.ToString("00");
+        }
+    }
+}
